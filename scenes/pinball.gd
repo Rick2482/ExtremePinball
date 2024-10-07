6a400d0d1db6b2b35f22aa73bb233589cc64520d
Rick <93201299+RicknCode@users.noreply.github.com>
2024-10-07 18:03:53 +0200

    Added score system, UI base, event bus and other things

---
+++ scenes/pinball.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@export var run_stats: RunStats
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	run_stats.score += 10
+ 
+func _on_ball_body_entered(body):
+	run_stats.score += 10
