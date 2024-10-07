2198d78497f7e5111a40fef496f504f5a9a5f467
Rick <93201299+RicknCode@users.noreply.github.com>
2024-10-07 18:50:09 +0200

    minor fix and now ball is replaced when lost

--- scenes/pinball.gd
+++ scenes/pinball.gd
@@ -2,15 +2,19 @@ extends Node2D
 
 @export var run_stats: RunStats
 
-# Called when the node enters the scene tree for the first time.
+const BALL_SCENE: PackedScene = preload("res://scenes/ball.tscn")
+
 func _ready():
 	run_stats.score += 10
 
-func new_ball():
-	pass
+func add_ball():
+	var new_ball = BALL_SCENE.instantiate()
+	new_ball.global_position = Vector2(1258, 212)
+	call_deferred("add_child", new_ball)
  
 func _on_ball_body_entered(body):
 	run_stats.score += 10
 
 func _on_exit_body_entered(body):
 	body.queue_free()
+	add_ball()
